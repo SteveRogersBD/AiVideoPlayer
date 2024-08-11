2a0d930d95192447bf9a0efff1c5ed3900154bf6
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-08-10 23:00:44 -0500

    left,right,space added

--- First.py
+++ First.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import mediapipe as mp
-
+import handGestures as HG
+import time
+import pyautogui as pg
 # Initialize Mediapipe Hands module
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -8,7 +10,12 @@ h = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5)
 
 # Open video capture
 cap = cv.VideoCapture(0)
-handPos={}
+handPos = {}
+pressed = False
+rHand=False
+lHand=False
+last_press_time = 0
+
 while True:
     r, frame = cap.read()
     if r:
@@ -21,13 +28,37 @@ while True:
         res = h.process(frame)
         frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
+        current_time = time.time() #current
+        if pressed and (current_time - last_press_time) >= 2:
+            pressed = False
+
         if res.multi_hand_landmarks:
-            for lm in res.multi_hand_landmarks:
-                myHand=res.multi_hand_landmarks[0]
+            for lm,hand_handedness in zip(res.multi_hand_landmarks,res.multi_handedness):
+                myHand = res.multi_hand_landmarks[0]
+                handedness = hand_handedness.classification[0].label
+                if handedness=='right': rHand = True
+                elif handedness=='left': lHand = True
                 for id, point in enumerate(myHand.landmark):
                     height, width, _ = frame.shape
                     cx, cy = int(point.x * width), int(point.y * height)  # Convert coordinates
-                    handPos[id]={cx,cy}
+                    handPos[id] = (cx, cy)
+
+                if HG.pressSpace(handPos,handedness) and not pressed:
+                    print('space pressed')
+                    pg.press('space')
+                    pressed = True
+                    last_press_time = current_time
+
+                if HG.pressLeft(handPos,handedness) and not pressed:
+                    print('left pressed')
+                    pg.press('left')
+                    pressed = True
+                    last_press_time = current_time
+                if HG.pressRight(handPos,handedness) and not pressed:
+                    print('right pressed')
+                    pg.press('right')
+                    pressed=True
+                    last_press_time = current_time
                 mp_drawing.draw_landmarks(frame, lm, mp_hands.HAND_CONNECTIONS)
 
         cv.imshow('frame', frame)
@@ -35,7 +66,7 @@ while True:
         # Exit if 'q' is pressed
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
-print(handPos)
+
 # Release resources
 cap.release()
 cv.destroyAllWindows()
---
+++ handGestures.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import mediapipe as mp
+
+
+def pressSpace(handPos,handedness):
+    # Retrieve coordinates using .get() to avoid KeyError
+    x8, y8 = handPos.get(8, (0, 0))
+    x6, y6 = handPos.get(6, (0, 0))
+    x12, y12 = handPos.get(12, (0, 0))
+    x10, y10 = handPos.get(10, (0, 0))
+    x16, y16 = handPos.get(16, (0, 0))
+    x14, y14 = handPos.get(14, (0, 0))
+    x20, y20 = handPos.get(20, (0, 0))
+    x18, y18 = handPos.get(18, (0, 0))
+    x4,y4 = handPos.get(4, (0, 0))
+    x0,y0 = handPos.get(0, (0, 0))
+    flag=False
+    if handedness == 'Right' and x4>=x0: flag=True
+    elif handedness == 'Left' and x0>=x4: flag=True
+    # Check if the thumb and other landmarks meet the conditions
+    if y8 > y6 and y12 > y10 and y16 > y14 and y20 > y18 and flag:
+        return True
+    return False
+def pressRight(handPos,handedness):
+    if handedness == 'Left':
+        x8, y8 = handPos.get(8, (0, 0))
+        x6, y6 = handPos.get(6, (0, 0))
+        x12, y12 = handPos.get(12, (0, 0))
+        x10, y10 = handPos.get(10, (0, 0))
+        x16, y16 = handPos.get(16, (0, 0))
+        x14, y14 = handPos.get(14, (0, 0))
+        x20, y20 = handPos.get(20, (0, 0))
+        x18, y18 = handPos.get(18, (0, 0))
+        x4,y4 = handPos.get(4, (0, 0)) #thumbtip
+        x5,y5 = handPos.get(5, (0, 0))
+        if y8 > y6 and y12 > y10 and y16 > y14 and y20 > y18 and x4>x5:
+            return True
+    else : return False
+
+def pressLeft(handPos,handedness):
+    if handedness == 'Right':
+        x8, y8 = handPos.get(8, (0, 0))
+        x6, y6 = handPos.get(6, (0, 0))
+        x12, y12 = handPos.get(12, (0, 0))
+        x10, y10 = handPos.get(10, (0, 0))
+        x16, y16 = handPos.get(16, (0, 0))
+        x14, y14 = handPos.get(14, (0, 0))
+        x20, y20 = handPos.get(20, (0, 0))
+        x18, y18 = handPos.get(18, (0, 0))
+        x4,y4 = handPos.get(4, (0, 0)) #thumbtip
+        x5,y5 = handPos.get(5, (0, 0))
+        if y8 > y6 and y12 > y10 and y16 > y14 and y20 > y18 and x4<x5:
+            return True
+    else : return False
